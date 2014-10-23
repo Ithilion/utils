48f8a1be05c44beda1335365e0e875796d1304df
Ithilion <3318892+Ithilion@users.noreply.github.com>
2014-10-23 03:01:47 +0200

    oqueue_update: removed as obsolete
    passgen: switched to pyperclip for copy-to-clipboard functionality

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # utils
 A collection of various utilities I'll keep writing over time
 
- - `oqueue_update.py` oQueue WoW addon updater.
  - `passgen.py` A cryptographically (i think) secure password generator.
  - `qpftoxml.py` Converts a `.qpf` frame list to a `.xml` Matroska chapters file; useful for fansubber encoders.
--- passgen.py
+++ passgen.py
@@ -1,7 +1,6 @@
 import random
 import argparse
-import os
-from tkinter import Tk
+import pyperclip
 
 parser = argparse.ArgumentParser(description="A cryptographically (i think) secure password generator")
 parser.add_argument("-n", type = int, choices = list(range(8,257)), default = 16, help = "number of generated charaters; valid range is 8-256; default is 16", metavar="")
@@ -19,10 +18,5 @@ muhpass = ''
 for i in range(args.n):
 	muhpass += chr(random.SystemRandom().choice(passrange))
 
-r = Tk()
-r.withdraw()
-r.clipboard_clear()
-r.clipboard_append(muhpass)
-r.destroy()
-
+pyperclip.copy(muhpass)
 print("copied password to clipboard")
