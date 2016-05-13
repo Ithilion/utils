7b2ea220134f3d34c284b22750c9a25e5f11a9c4
Ithilion <3318892+Ithilion@users.noreply.github.com>
2016-05-13 03:54:58 +0200

    passgen.py: added batch passwords

--- passgen.py
+++ passgen.py
@@ -4,7 +4,8 @@ import random
 import pyperclip
 
 parser = argparse.ArgumentParser(description="A cryptographically secure (i think) password generator")
-parser.add_argument("-n", type = int, choices = list(range(8,257)), default = 16, metavar="", help = "number of generated charaters; valid range is 8-256; default is 16")
+parser.add_argument("-l", type = int, choices = list(range(8,257)), default = 16, metavar="", help = "length of the generated password; valid range is 8-256; default is 16")
+parser.add_argument("-n", type = int, default = 1, help = "number of generated passwords")
 parser.add_argument("-t", choices = ["an", "noalt", "full"], default = "an", metavar="", help = "type of generated characters; valid input is 'an' (alphanumeric), 'noalt' (all printable ASCII characters except space, ` and ~) or 'full' (all printable ASCII characters except space); default is 'an'")
 args = parser.parse_args()
 
@@ -15,9 +16,14 @@ elif args.t == "noalt":
 elif args.t == "full":
 	passrange = list(range(33, 127))
 
-muhpass = ''
+finalpass = ""
+newline = ""
 for i in range(args.n):
-	muhpass += chr(random.SystemRandom().choice(passrange))
+	temppass = ""
+	for i in range(args.l):
+		temppass += chr(random.SystemRandom().choice(passrange))
+	finalpass += newline + temppass
+	newline = "\n"
 
-pyperclip.copy(muhpass)
+pyperclip.copy(finalpass)
 print("password copied to clipboard")
